1e9e49a281ce8fa13c07ceb625aa4951b95727cd
Gabriel Braga <braga_gabriel@live.com>
2025-03-19 19:33:42 -0300

    primeiro commit

---
+++ src/controlebancario/ControleBancario.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package controlebancario;
+import view.menu;
+
+
+/**
+ *
+ * @author braga
+ */
+public class ControleBancario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        menu menuPrincipal = new menu();
+        menuPrincipal.setVisible(true);
+        
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/model/conta.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author braga
+ */
+public class conta {
+    
+    private int numeroConta;
+    private double saldo;
+
+    public conta() {
+    }
+
+    public conta(int numeroConta, double saldo) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    
+    
+}
